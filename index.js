5c671fde589b9716384ae33db89b9feb0cf869f4
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-06-20 11:54:23 +0300

    connect to mongo in index file and update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const app = require("./app");
+const { connectToMongo } = require("./db/mongo-connection");
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
 	console.log(`app listening on ${PORT}`);
+	connectToMongo();
 });
