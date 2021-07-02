7eb5e5624bd55505242f52fb32ab0b31f393776a
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-07-02 21:10:08 +0300

    add /diff route to api

--- utils/image-diff/index.js
+++ utils/image-diff/index.js
@@ -29,8 +29,13 @@ const getDiffFromUrl = async (s3Url1, s3Url2) => {
 	// const img2 = PNG.sync.read(fs.readFileSync("headlines/n12/2021-07-01_21-15.png"));
 	const { width: width1, height: height1 } = sizeOf(data1);
 	const { width: width2, height: height2 } = sizeOf(data2);
+
 	console.log("width1: ", width1, "height1: ", height1);
 	console.log("width2: ", width2, "height2: ", height2);
+
+	if (width1 !== width2 || height1 !== height2) {
+		throw new Error("Different image dimensions!");
+	}
 
 	const dataSharp1 = await sharp(data1).png().toBuffer();
 	const dataSharp2 = await sharp(data2).png().toBuffer();
@@ -40,10 +45,14 @@ const getDiffFromUrl = async (s3Url1, s3Url2) => {
 	const diff = new PNG({ width: width1, height: height1 });
 
 	const diffNum = pixelmatch(img1.data, img2.data, diff.data, width1, height1, { threshold: 0.1 });
-	console.log(diffNum);
-	console.log((diffNum / (width1 * height1)) * 100);
+	const diffPercentage = (diffNum / (width1 * height1)) * 100;
+
+	console.log("diffNum: ", diffNum);
+	console.log("diffPercentage: ", diffPercentage);
 
 	fs.writeFileSync(`ignore/diff.png`, PNG.sync.write(diff));
+
+	return { diffNum, diffPercentage, diff };
 };
 
 module.exports = { getDiffFromUrl };
